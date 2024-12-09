d2b95193fa97f48df8025b2c8a6b3947da39803d
Leejoongjae123 <ljj3347@naver.com>
2024-12-09 22:21:26 +0900

    '1'

--- app/protected/components/ChartUI.js
+++ app/protected/components/ChartUI.js
@@ -2,19 +2,13 @@
 import React from "react";
 import { Select, SelectItem } from "@nextui-org/react";
 import {Button, ButtonGroup} from "@nextui-org/button";
+import { IoMdDownload } from "react-icons/io";
+
 import Chart from "./Chart";
 function ChartUI() {
   return (
     <div className="w-full flex flex-col gap-8">
       <div className="flex-1 flex gap-4">
-        <div className="flex-1 flex flex-col gap-4">
-          <h1 className="text-2xl font-bold">분류</h1>
-          <Select className="w-full" label="분류">
-            <SelectItem key="1">1</SelectItem>
-            <SelectItem key="2">2</SelectItem>
-            <SelectItem key="3">3</SelectItem>
-          </Select>
-        </div>
         <div className="flex-1 flex flex-col gap-4">
           <h1 className="text-2xl font-bold">상품명</h1>
           <Select className="w-full" label="상품명">
@@ -23,16 +17,43 @@ function ChartUI() {
             <SelectItem key="3">3</SelectItem>
           </Select>
         </div>
+        {/* <div className="flex-1 flex flex-col gap-4">
+          <h1 className="text-2xl font-bold">상품명</h1>
+          <Select className="w-full" label="상품명">
+            <SelectItem key="1">1</SelectItem>
+            <SelectItem key="2">2</SelectItem>
+            <SelectItem key="3">3</SelectItem>
+          </Select>
+        </div> */}
       </div>
 
       <div className="flex-1 flex flex-col">
         <div className="flex justify-between items-center">
         <h1 className="text-2xl font-bold">그래프</h1>
         
-        <Button color="primary">다운로드</Button>
+        <Button startContent={<IoMdDownload className="text-medium text-white"/>} color="primary">이미지</Button>
         </div>
         <Chart></Chart>
         
+      </div>
+
+      <div className="flex-1 flex gap-4">
+        <div className="flex-1 flex flex-col gap-4">
+          <h1 className="text-2xl font-bold">용도</h1>
+          <Select className="w-full" label="상품명">
+            <SelectItem key="1">1</SelectItem>
+            <SelectItem key="2">2</SelectItem>
+            <SelectItem key="3">3</SelectItem>
+          </Select>
+        </div>
+        {/* <div className="flex-1 flex flex-col gap-4">
+          <h1 className="text-2xl font-bold">상품명</h1>
+          <Select className="w-full" label="상품명">
+            <SelectItem key="1">1</SelectItem>
+            <SelectItem key="2">2</SelectItem>
+            <SelectItem key="3">3</SelectItem>
+          </Select>
+        </div> */}
       </div>
     </div>
   );
